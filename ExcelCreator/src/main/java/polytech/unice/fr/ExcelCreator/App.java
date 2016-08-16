91883e8d0f215ee242ce9651b3369e3281966d0f
= <cappe@polytech.unice.fr>
2016-08-16 11:18:26 +0200

    first commit

---
+++ ExcelCreator/src/main/java/polytech/unice/fr/ExcelCreator/App.java
@@ -0,0 +1,13 @@
+package polytech.unice.fr.ExcelCreator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
