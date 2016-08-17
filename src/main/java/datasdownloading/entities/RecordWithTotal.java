911947e4259bef7dee2ecb98e05e83427e90abfa
Thibaut SORIANO <st405262@etu.unice.fr>
2016-08-17 15:11:58 +0200

    d'la bonne graisse

---
+++ src/main/java/datasdownloading/entities/RecordWithTotal.java
@@ -0,0 +1,21 @@
+package main.java.datasdownloading.entities;
+
+public class RecordWithTotal extends Record {
+
+	private int totalImp;
+
+	public int getTotalImp() {
+		return totalImp;
+	}
+
+	public void setTotalImp(int totalImp) {
+		this.totalImp = totalImp;
+	}
+
+	public RecordWithTotal(int totalImp) {
+		super();
+		this.totalImp = totalImp;
+	}
+	
+	
+}
